b9a1f0e023ce51408342fd277d6e96a6cc4cea96
phpstorm <jc675@njit.edu>
2018-04-25 23:32:26 -0400

    Added Navigation

--- App.js
+++ App.js
@@ -5,54 +5,45 @@
  */
 
 import React, { Component } from 'react';
+import {StackNavigator} from "react-navigation";
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Dimensions
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Index from "./src/components/index";
+import Note from "./src/components/index";
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
+
+class App extends Component {
+    
+
+
+    render() {
+    
+    const MainNavigator = StackNavigator({
+        Notes: {screen: Index},
+        Note: {screen: Note}
+    });
+        
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+            <View style={styles.container}>
+                
+                <MainNavigator style={{ width: Dimensions.get('window').width }}/>
+ 
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+                container: {
+                     flex: 1,
+                     backgroundColor: '#fff'
+                            }       
+                            });
+
+export default App;
